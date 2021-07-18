54b82594d532bd76ff4c14ff6d844e4eb79f8eec
Gustavo Corrêa Mendes <67390202+gustavomendesof@users.noreply.github.com>
2021-07-18 17:12:32 -0300

    Atualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'Card.dart';
 import 'Produto.dart';
-import 'teste.dart';
+import 'Carta.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: teste(),
+    home: Carta(),
     //home: Produto(),
   ));
 }
